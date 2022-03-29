2cda63e0a1b1080e4927cf4f8371b930acb72928
sehdez <sergio.hdez.cuevas@gmail.com>
2022-03-28 18:04:49 -0600

    inicio de la seccion

---
+++ tabla.js
@@ -0,0 +1,9 @@
+console.clear();
+
+console.log('*****************');
+console.log('*  Tabla del 5  *');
+console.log('*****************');
+const base = 5;
+for ( let i = 1; i <= 10; i++) {
+    console.log ( ` ${ base } * ${ i } = ${ i * 5 }` );
+}
